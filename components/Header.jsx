384937078c2c55cf8efb73bb1beabf3d5c75e601
FrankieSlinn <frankissling@yahoo.co.uk>
2025-01-15 11:41:48 +0000

    Initialise

---
+++ components/Header.jsx
@@ -0,0 +1,30 @@
+import {Text, View } from "react-native";
+// import {s} from "../App.style.js";
+
+// import {Logo} from "./Logo.js";
+// import {HelpIcon} from "./HelpIcon";
+// import {StatsIcon} from "./StatsIcon";
+export function Header(){
+    
+
+
+return <>
+<View>
+
+// {/* <HelpIcon
+// content = {content}
+// setContent = {setContent}
+// onPress={() => setIcon("help")}/>
+// <Logo/>
+// <StatsIcon
+// content = {content}
+// setContent = {setContent}
+// onPress={() => setIcon("stats")}
+// // /> */}
+<Text>Header</Text>
+
+</View>
+
+</>
+
+}

04aa8dab296a7354e5068339750d52f5c3ce381a
FrankieSlinn <frankissling@yahoo.co.uk>
2025-06-30 19:25:26 +0100

    get works

--- backend/models/hofdata.js
+++ backend/models/hofdata.js
@@ -6,11 +6,13 @@ const hofdataSchema = new mongoose.Schema({
   handle: { type: String, required: false },
   score: { type: String, required: true }
 }, {
-  timestamps: true // Adds createdAt and updatedAt
+  timestamps: true,
+  collection: 'WordDominoesHOF2025Collection'  
 });
 
 // Compile model
 const HOFData = mongoose.model('HOFData', hofdataSchema);
+
 
 // Export model
 module.exports = HOFData;
